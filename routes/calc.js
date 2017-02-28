241163ad520162ebb10ce3c4e699b53c870bf7ef
gaqg <gaqg.13@hotmail.com>
2017-02-28 14:16:17 -0700

    third commit

--- routes/calc.js
+++ routes/calc.js
@@ -38,4 +38,41 @@ router.get('/:v1/:v2', function(req, res, next) {
   res.render('calcular', {resu: resultado});
 });
 
+router.get('/:v1:op:v2:op2:v3', function(req, res, next) {
+	var var1= req.params.v1;
+	var operator= req.params.op;
+	var var2= req.params.v2;
+	
+	var operator2= req.params.op2;
+	var var3= req.params.v3;
+	var resultado = 0;
+
+
+	switch(operator){
+		case "+":
+		resultado=Number(var1)+Number(var2);
+		break;
+		case "-":
+		resultado=var1-var2;
+		break;
+		case "*":
+		resultado=var1*var2;
+		break;
+
+	}
+	switch(operator2){
+		case "+":
+		resultado=Number(resultado)+Number(var3);
+		break;
+		case "-":
+		resultado=resultado-var3;
+		break;
+		case "*":
+		resultado=resultado*var3;
+		break;
+
+	}
+  res.render('calcular', {resu: resultado});
+});
+
 module.exports = router;
